43a2ca42580aeda2b4a8c16e8b2de39b267895c8
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 16:05:47 -0700

    Update stochastic_collission.jl

--- stochastic_collission.jl
+++ stochastic_collission.jl
@@ -1,10 +1,17 @@
+###########################################################
+# LOAD MODULES                                            #
+###########################################################
+
 using Plots
 using Random
 
 include("lib/maxwell_boltzman.jl")
 using .MaxwellBoltzmann
 
-# Configuration of the simulation
+###########################################################
+# CONFIGURATION                                           #
+###########################################################
+
 temp_1_k = 50.0
 temp_2_k = 300.0
 mass_amu = 32.0
@@ -12,14 +19,15 @@ steps = 100
 N = 1000000
 fraction_atoms_reassigned_per_step = 0.1
 
-# Array of each step's temperature
-temps = collect(range(temp_1_k, temp_2_k, steps))
+###########################################################
+# SIMULATION                                              #
+###########################################################
 
 # Each of row of the following array is the ensemble at the corresponding step
 ensembles = zeros(Float64, steps, N)
 
 # Assign the velcoties of the first ensemble at the starting temperature
-ensembles[1, :] = maxwell_rvs(temps[1], mass_amu, N)
+ensembles[1, :] = maxwell_rvs(temp_1_k, mass_amu, N)
 println("Step 1 complete")
 
 # Go through each subsequent ensemble (represented as a row in ensembles array), 
@@ -30,10 +38,15 @@ num_atoms_to_reassign = Int64(ceil(N * fraction_atoms_reassigned_per_step))
 for row in range(2, steps)
     ensembles[row, :] = copy(ensembles[row-1, :])
     atoms_to_reassign = randperm(length(ensembles[row, :]))[1:num_atoms_to_reassign]
-    # ensembles[row, atoms_to_reassign] = maxwell_rvs(temps[row], mass_amu, num_atoms_to_reassign)
     ensembles[row, atoms_to_reassign] = maxwell_rvs(temp_2_k, mass_amu, num_atoms_to_reassign)
     println("Step $row complete")
 end
+
+###########################################################
+# PLOTTING                                                #
+###########################################################
+
+println("Generating before and after plot panels...")
 
 # Array of velocities for PDFs
 velocities = collect(range(0, 2000, 100))
@@ -51,3 +64,9 @@ plot_end = plot!(velocities, pdf_end, title="Distribution at $temp_1_k K", xlabe
 combined_plots = plot(plot_start, plot_end, layout=(1, 2), size=(600, 300), dpi=300)
 
 savefig(combined_plots, "output/stochastic_collision.png")
+
+###########################################################
+# DONE                                                    #
+###########################################################
+
+println("Done")
