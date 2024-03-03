2ad2d548b6bd5ae6a6ce1853c6ef75e43120bfa3
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 15:42:12 -0700

    Update stochastic_collission.jl

--- stochastic_collission.jl
+++ stochastic_collission.jl
@@ -45,8 +45,8 @@ pdf_end = maxwell_pdf(velocities, temp_2_k, mass_amu)
 histogram(ensembles[1,:], bins=100, normalize=true, alpha=0.3, color=:blue, label="First Step")
 plot_start = plot!(velocities, pdf_start, title="Distribution at $temp_1_k K", xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, alpha=1.0, label="PDF Start")
 
-histogram(ensembles[end,:], bins=100, normalize=true, alpha=0.3, color=:blue, label="End Step")
-plot_end = plot!(velocities, pdf_end, title="Distribution at $temp_1_k K", xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, alpha=1.0, label="PDF End")
+histogram(ensembles[end,:], bins=100, normalize=true, alpha=0.3, color=:orange, label="End Step")
+plot_end = plot!(velocities, pdf_end, title="Distribution at $temp_1_k K", xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:orange, alpha=1.0, label="PDF End")
 
 combined_plots = plot(plot_start, plot_end, layout=(1, 2), size=(600, 300), dpi=300)
 
