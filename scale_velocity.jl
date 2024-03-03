25fd2b1bacca99830f61f29f0ebc7bc20d31eb4f
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 10:18:14 -0700

    Update scale_velocity.jl

--- scale_velocity.jl
+++ scale_velocity.jl
@@ -20,5 +20,6 @@ end
 
 velcoities = collect(range(0, 2000, 100))
 ys = maxwell_boltzmann_pdf(velcoities, 300.0, 32.0)
-display(plot(velcoities, ys))
+pdf_plot = plot(velcoities, ys, title="Maxwell-Boltzmann Distribution", xlabel="v (m/s)", ylabel="P(v)")
+display(pdf_plot)
 readline()
