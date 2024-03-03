756dbefc9d2eca174bf91944687d262c87994676
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 14:18:04 -0700

    Update stochastic_collission.jl

--- stochastic_collission.jl
+++ stochastic_collission.jl
@@ -3,6 +3,7 @@ using Plots
 include("lib/maxwell_boltzman.jl")
 using .MaxwellBoltzmann
 
+# Configuration of the simulation
 temp_1_k = 50.0
 temp_2_k = 300.0
 mass_amu = 32.0
@@ -10,6 +11,11 @@ steps = 100
 N = 1000000
 fraction_atoms_reassigned_per_step = 0.1
 
+# Array of each steps temperature
+temps = collect(range(temp_1_k, temp_2_k, steps))
 
+# Each of row of the following array is the ensemble at the corresponding step
+ensembles = zeros(Float64, steps, N)
 
-mb_dists = 
+# Assign the velcoties of the first ensemble at the starting temperature
+ensembles[1, :] = maxwell_rvs(temps[1], mass_amu, N)
