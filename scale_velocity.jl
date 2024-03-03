2207005b9358c31f89c717de04140d1db927b31d
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 10:15:19 -0700

    Update scale_velocity.jl

--- scale_velocity.jl
+++ scale_velocity.jl
@@ -8,6 +8,17 @@ function scale(temp_k, mass_amu)
     sqrt(2*kb*temp_k/mass_amu/kg_per_amu)
 end
 
-function maxwell_boltzmann_pdf(vs, temp_k, mass_amu)
-    
+function maxwell_boltzmann_pdf(velocities, temp_k, mass_amu)
+    sigma = scale(temp_k, mass_amu)
+
+    function pdf(v)
+        sqrt(2/π)*(v^2*exp(-v^2/(2*sigma^2))) / sigma^3
+    end
+
+    pdf.(velocities)
 end
+
+velcoities = collect(range(0, 2000, 100))
+ys = maxwell_boltzmann_pdf(velcoities, 300.0, 32.0)
+display(plot(velcoities, ys))
+readline()
