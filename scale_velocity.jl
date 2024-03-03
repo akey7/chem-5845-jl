ba72d1958efb4da36782e9749458c4ad453e61a4
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-03 13:22:01 -0700

    Update scale_velocity.jl

--- scale_velocity.jl
+++ scale_velocity.jl
@@ -3,10 +3,19 @@ using Plots
 include("lib/maxwell_boltzman.jl")
 using .MaxwellBoltzmann
 
+temp_k_01 = 50.0
+temp_k_02 = 300.0
+o2_mass_amu = 32.0
 velocities = collect(range(0, 2000, 100))
-ys = maxwell_pdf(velocities, 300.0, 32.0)
-rvs = maxwell_rvs(300.0, 32.0, 1000000)
-histogram(rvs, bins=100, normalize=true, alpha=0.5, color=:orange, label="Random Values")
-pdf_plot = plot!(velocities, ys, title="Maxwell-Boltzmann Distribution", xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, label="PDF")
-display(pdf_plot)
+
+pdf_01 = maxwell_pdf(velocities, temp_k_01, o2_mass_amu)
+rvs_01 = maxwell_rvs(temp_k_01, o2_mass_amu, 1000000)
+
+lambda = sqrt(temp_k_02 / temp_k_01)
+pdf_02 = maxwell_pdf(velocities, temp_k_02, o2_mass_amu)
+rvs_02 = lambda .* rvs_01
+
+histogram(rvs_01, bins=100, normalize=true, alpha=0.3, color=:blue, label="Random Velocities")
+plot_01 = plot!(velocities, pdf_01, title="Distribution at $temp_k_01 K", xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, alpha=1.0, label="PDF")
+display(plot_01)
 readline()
