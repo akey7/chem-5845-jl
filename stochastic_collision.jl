950702078327ee3fcf577cc67708f24149e621b0
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-04 11:59:40 -0700

    Change all references of velocity to speed.

--- stochastic_collision.jl
+++ stochastic_collision.jl
@@ -14,8 +14,8 @@ using .MaxwellBoltzmann
 
 temp_1_k = 50.0
 temp_2_k = 300.0
-min_velocity = 0.0
-max_velocity = 2000.0
+min_speed = 0.0
+max_speed = 2000.0
 mass_amu = 32.0
 steps = 50
 N = 1000000
@@ -33,7 +33,7 @@ ensembles[1, :] = maxwell_rvs(temp_1_k, mass_amu, N)
 println("Step 1 complete")
 
 # Go through each subsequent ensemble (represented as a row in ensembles array), 
-# randomly reassigning velocities at each step
+# randomly reassigning speeds at each step
 
 num_atoms_to_reassign = Int64(ceil(N * fraction_atoms_reassigned_per_step))
 
@@ -50,21 +50,21 @@ end
 
 println("Generating before and after plot panels...")
 
-# Array of velocities for PDFs
-velocities = collect(range(min_velocity, max_velocity, 1000))
+# Array of speeds for PDFs
+speeds = collect(range(min_speed, max_speed, 1000))
 
 # Plot the start and end distributions side-by-side
-pdf_start = maxwell_pdf(velocities, temp_1_k, mass_amu)
-pdf_end = maxwell_pdf(velocities, temp_2_k, mass_amu)
+pdf_start = maxwell_pdf(speeds, temp_1_k, mass_amu)
+pdf_end = maxwell_pdf(speeds, temp_2_k, mass_amu)
 
 # Calculate the upper y limit
 max_y = maximum(pdf_start) * 1.1
 
 histogram(ensembles[1,:], bins=100, normalize=true, alpha=0.3, color=:blue, label="First Step")
-plot_start = plot!(velocities, pdf_start, title="Distribution at $temp_1_k K", ylim=(0, max_y), xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, alpha=1.0, label="PDF Start")
+plot_start = plot!(speeds, pdf_start, title="Distribution at $temp_1_k K", ylim=(0, max_y), xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:blue, alpha=1.0, label="PDF Start")
 
 histogram(ensembles[end,:], bins=100, normalize=true, alpha=0.3, color=:orange, label="End Step")
-plot_end = plot!(velocities, pdf_end, title="Distribution at $temp_2_k K", ylim=(0, max_y), xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:orange, alpha=1.0, label="PDF End")
+plot_end = plot!(speeds, pdf_end, title="Distribution at $temp_2_k K", ylim=(0, max_y), xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:orange, alpha=1.0, label="PDF End")
 
 combined_plots = plot(plot_start, plot_end, layout=(1, 2), size=(600, 300), dpi=300)
 
@@ -77,9 +77,9 @@ anim = Animation()
 
 # Generate each frame
 for row in 1:steps
-    histogram(ensembles[row,:], ylim=(0, max_y), xlim=(min_velocity, max_velocity), bins=50, normalize=true, alpha=0.3, color=:gray, label="Step $row")
-    plot!(velocities, pdf_start, linewidth=3, color=:blue, alpha=1.0, label="Start PDF")
-    plot!(velocities, pdf_end, title="Step $row", ylim=(0, max_y), xlim=(min_velocity, max_velocity), xlabel="v (m/s)", ylabel="P(v)", linewidth=3, color=:orange, alpha=1.0, label="End PDF")
+    histogram(ensembles[row,:], ylim=(0, max_y), xlim=(min_speed, max_speed), bins=50, normalize=true, alpha=0.3, color=:gray, label="Step $row")
+    plot!(speeds, pdf_start, linewidth=3, color=:blue, alpha=1.0, label="Start PDF")
+    plot!(speeds, pdf_end, title="Step $row", ylim=(0, max_y), xlim=(min_speed, max_speed), xlabel="speed (m/s)", ylabel="P(v)", linewidth=3, color=:orange, alpha=1.0, label="End PDF")
     frame(anim)
 
     if row % 10 == 0
