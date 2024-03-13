6e0e5d526fc52fbab3abcfe29943b903b2d5d5cf
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-13 11:34:09 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,5 +12,7 @@ end
 @testset "Test GBM initialization" begin
     gbm = setup_test_environmment()
     println(gbm)
-    @test all(isapprox.(gbm[:ys], [9.531, 7.955, 4.9424], atol=0.01))
+    @test all(isapprox.(gbm[:rs], [0.652, 1.098, 0.437], atol=0.01))
+    @test all(isapprox.(gbm[:xs], [8.942, 3.531, 3.943], atol=0.01))
+    @test all(isapprox.(gbm[:ys], [9.531, 7.955, 4.942], atol=0.01))
 end 
