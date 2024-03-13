74c46f6e7798a349b483775e2255c9c386b1e852
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-13 11:21:26 -0600

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,16 @@
 using Test
+using Random
 
+include("../src/generalized_born.jl")
+using .GeneralizedBornModel
 
+function setup_test_environmment()
+    Random.seed!(1234)
+    init_model(10, 10, 2, 3)
+end
+
+@testset "Test GBM initialization" begin
+    gbm = setup_test_environmment()
+    println(gbm)
+    @test all(isapprox.(gbm[:ys], [9.531, 7.955, 4.9424], atol=0.01))
+end 
