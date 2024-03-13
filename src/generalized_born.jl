1c7b9f5370a0245a4aadbe735fca43184b7cb4bc
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-13 11:15:59 -0600

    Update generalized_born.jl

--- src/generalized_born.jl
+++ src/generalized_born.jl
@@ -2,11 +2,18 @@
 # TWO-DIMENSIONAL GENERALIZED BORN MODEL                  #
 ###########################################################
 
-module GeneralizedBorn
+module GeneralizedBornModel
+
+using Random
+
+export init_model
 
 # Model starts at origin 0, 0
-function init_model()
-
+function init_model(max_x, max_y, max_radius, num)
+    rs = max_radius .* rand(num)
+    xs = max_x .* rand(num)
+    ys = max_y .* rand(num)
+    Dict(:rs => rs, :xs => xs, :ys => ys)
 end
 
 end
