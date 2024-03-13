10ac689e7c4bff67b20d4fad4a17a930bd306c93
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-13 10:40:13 -0600

    Create generalized_born.jl

---
+++ src/generalized_born.jl
@@ -0,0 +1,12 @@
+###########################################################
+# TWO-DIMENSIONAL GENERALIZED BORN MODEL                  #
+###########################################################
+
+module GeneralizedBorn
+
+# Model starts at origin 0, 0
+function init_model()
+
+end
+
+end
