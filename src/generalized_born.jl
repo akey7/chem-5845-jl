4ef70a1b77ed31b08f47b269ccc7fa98f343dd00
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-13 20:54:12 -0600

    Add model plotting code

--- src/generalized_born.jl
+++ src/generalized_born.jl
@@ -5,8 +5,9 @@
 module GeneralizedBornModel
 
 using Random
+using Plots
 
-export init_model
+export init_model, plot_model
 
 # Model starts at origin 0, 0
 function init_model(max_x, max_y, max_radius, num)
@@ -14,6 +15,11 @@ function init_model(max_x, max_y, max_radius, num)
     xs = max_x .* rand(num)
     ys = max_y .* rand(num)
     Dict(:rs => rs, :xs => xs, :ys => ys)
+end
+
+# Creates a scatter plot of the circles in the model
+function plot_model(model)
+    scatter(model[:xs], model[:ys], markersize=model[:rs]*10)
 end
 
 end
