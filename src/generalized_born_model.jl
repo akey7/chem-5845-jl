ec8c171fdc5cab294a25aaf6a7b5cf58d78d166b
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-15 15:39:34 -0600

    Return, test, and visualize model as Dict.

--- src/generalized_born_model.jl
+++ src/generalized_born_model.jl
@@ -12,9 +12,11 @@ export init_model
 function init_model(radius::Float64, row_and_col_count::Int, size_x::Float64, size_y::Float64)
     rows = collect(range(start=radius, stop=size_y, step=size_y / row_and_col_count))
     cols = collect(range(start=radius, stop=size_x, step=size_x / row_and_col_count))
-    grid_matrix = [(row, col) for row in rows, col in cols]
-    grid_vector = reshape(grid_matrix, length(rows) * length(cols))
-    [t[i] for t in grid_vector, i in 1:2]
+    grid_matrix_1 = [(row, col) for row in rows, col in cols]
+    grid_vector = reshape(grid_matrix_1, length(rows) * length(cols))
+    grid_matrix_2 = [t[i] for t in grid_vector, i in 1:2]
+
+    Dict(:grid => grid_matrix_2, :r => radius)
 end
 
 end
