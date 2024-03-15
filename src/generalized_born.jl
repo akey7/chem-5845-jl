5cefeab4724672792645e4deba54d3a2c891bfc7
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-15 13:21:53 -0600

    Refactor grid calculation

--- src/generalized_born.jl
+++ src/generalized_born.jl
@@ -9,11 +9,11 @@ using Random
 export init_model
 
 # Model starts at origin 0, 0
-function init_model(radius::Float64, row_count::Int64, col_count::Int64)
-    rows = collect(range(start=radius, stop=row_count * radius, step=radius))
-    cols = collect(range(start=radius, stop=col_count * radius, step=radius))
+function init_model(radius::Float64, spacing::Float64, size_x::Float64, size_y::Float64)
+    rows = collect(range(start=radius, stop=size_y, step=size_y / spacing))
+    cols = collect(range(start=radius, stop=size_x, step=size_x / spacing))
     grid_matrix = [(row, col) for row in rows, col in cols]
-    grid_vector = reshape(grid_matrix, row_count * col_count)
+    grid_vector = reshape(grid_matrix, length(rows) * length(cols))
     [t[i] for t in grid_vector, i in 1:2]
 end
 
