d52b21eb4bb5a5d0b8d3bdfa9693c1eafa16a7cd
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-15 20:20:48 -0600

    Rename rs to as

--- src/generalized_born_model.jl
+++ src/generalized_born_model.jl
@@ -15,27 +15,27 @@ function init_model(radius::Float64, row_and_col_count::Int, size_x::Float64, si
     grid_matrix_1 = [(row, col) for row ∈ rows, col ∈ cols]
     grid_vector = reshape(grid_matrix_1, length(rows) * length(cols))
     xs = [t[i] for t in grid_vector, i in 1:2]
-    rs = radius .+ zeros(Float64, length(xs[:, 1]))
+    as = radius .+ zeros(Float64, length(xs[:, 1]))
 
-    Dict(:xs => xs, :rs => rs)
+    Dict(:xs => xs, :as => as)
 end
 
-# # Calculate ΔG of solvation, assuming no overlap between spheres
-# function delta_g_solv(model::Dict, epsilon::Float64)
-#     xs = model[:xs]
-#     rs 
-#     n = length(xs[:, 1])
+# Calculate ΔG of solvation, assuming no overlap between spheres
+function delta_g_solv(model::Dict, epsilon::Float64)
+    xs = model[:xs]
+    as = model[:as]
+    n = length(xs[:, 1])
     
-#     a = -0.5 * (1 - 1/epsilon)
-#     b = 0
+    a = -0.5 * (1 - 1/epsilon)
+    b = 0
 
-#     for i ∈ 1:n-1
-#         for j ∈ i+1:n
+    for i ∈ 1:n-1
+        for j ∈ i+1:n
             
-#         end
-#     end
+        end
+    end
 
-#     c = sum()
-# end
+    c = sum()
+end
 
 end
