a23f70855cebe6842ffab643fb8297471b431db0
Alicia Key <2609896+akey7@users.noreply.github.com>
2024-03-15 20:35:48 -0600

    Test radius calculation.

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,12 @@ using .GeneralizedBornModel
 
 function setup_test_environmment()
     radius = 5.0
+    charge = 1.0
     row_and_col_count = 2
     size_x = 500.0
     size_y = 500.0
 
-    init_model(radius, row_and_col_count, size_x, size_y)
+    init_model(radius, charge, row_and_col_count, size_x, size_y)
 end
 
 @testset "Test GBM initialization" begin
@@ -20,4 +21,9 @@ end
 
     @test all(isapprox.(centers_x, [5.0, 255.0, 5.0, 255.0], atol=0.01))
     @test all(isapprox.(centers_y, [5.0, 5.0, 255.0, 255.0], atol=0.01))
-end 
+end
+
+@testset "Test rij calculation" begin
+    xs = [1.0 1.0; 2.0 2.0]
+    @test isapprox(rij(xs, 1, 2), 1.414, atol=0.01)
+end
